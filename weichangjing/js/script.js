5950218a7c0b4db243f50d8a5efb6001956b3ba7
北乐 <2696733529@qq.com>
2017-12-11 15:18:55 +0800

    第一屏动画修改

--- weichangjing/js/script.js
+++ weichangjing/js/script.js
@@ -8,4 +8,15 @@ var swiper = new Swiper('.swiper-container', {
         nextEl: '.swiper-button-next',
     },
 
+    //animate 动画
+    on:{
+        init: function(){
+            swiperAnimateCache(this); //隐藏动画元素
+            swiperAnimate(this); //初始化完成开始动画
+        },
+        slideChangeTransitionEnd: function(){
+            swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
+        }
+    }
+
 });
