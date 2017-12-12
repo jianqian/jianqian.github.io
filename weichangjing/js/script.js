6c52bc0024545d7704dc6d978e94840108eb12dd
北乐 <2696733529@qq.com>
2017-12-12 16:13:21 +0800

    修改音乐

--- weichangjing/js/script.js
+++ weichangjing/js/script.js
@@ -18,5 +18,17 @@ var swiper = new Swiper('.swiper-container', {
             swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
         }
     }
-
 });
+//音乐
+var mp3 = document.querySelector("#mp3");
+var mp3btn=document.querySelector("#mp3btn");
+mp3btn.addEventListener("click",function(){
+    if(mp3.paused)
+    {
+        mp3.play();
+        mp3btn.style.animationPlayState="running"
+    }else{
+        mp3.pause();
+        mp3btn.style.animationPlayState="paused"
+    }
+})
