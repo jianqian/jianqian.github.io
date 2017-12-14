1c6cc43e0d569a8a9859564cc18779a05f60b91e
北乐 <2696733529@qq.com>
2017-12-14 11:17:04 +0800

    添加第二屏

--- yaoqinghan/js/script.js
+++ yaoqinghan/js/script.js
@@ -1,10 +1,19 @@
-var swiper = new Swiper('.swiper-container', {
+var mySwiper = new Swiper ('.swiper-container', {
     direction: 'vertical',
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
     },
-});
+    on:{
+        init: function(){
+            swiperAnimateCache(this); //隐藏动画元素
+            swiperAnimate(this); //初始化完成开始动画
+        },
+        slideChangeTransitionEnd: function(){
+            swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
+        }
+    }
+})
 
 
 
